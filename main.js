9062f4310f13b1a06b52ed08be1b063f03cd6212
Kane <LisaxKane@gmail.com>
2022-10-09 22:30:32 -0500

    change grid color on hover implemented

--- main.js
+++ main.js
@@ -8,18 +8,18 @@ body.appendChild(content);
 
 let height = window.innerWidth;
 
-function makebox(column,row){
+function makebox(column){
 //create an empty div or column
 
-for ( let i = 0; i < row; i++) {
+for ( let i = 0; i < column; i++) {
     const emptydiv  = document.createElement('div');
     emptydiv.classList.add('container' + i);
-    emptydiv.textContent = i + 1
-    body.appendChild(emptydiv);
+    
+    content.appendChild(emptydiv);
  
 }
     
-
+pxsize = 480 / column ;
 
 // create 1 row of boxes
 
@@ -29,16 +29,21 @@ for ( let i = 0; i < column; i++) {
     const gridbox = document.createElement('divv');
     gridbox.classList.add('gridbox');
     gridbox.textContent = i + 1
-    
+    gridbox.style.height = pxsize + 'px';
+    gridbox.style.width = pxsize + 'px';
     cont1.appendChild(gridbox);
     
+    gridbox.addEventListener('mouseover',(event) => {
+        gridbox.style.backgroundColor = 'red'
+    });
     }
 }
-function makerow(row,column){
-makebox(column,column)
-for (let i = 1; i < row; i++) {
+function makerow(column){
+makebox(column)
+for (let i = 1; i < column; i++) {
   makebox(column)
   
 }
 }
-makerow(4,4)
+makerow(16)
+
